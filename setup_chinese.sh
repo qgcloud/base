f6f8a2cd1f61238e643079e081b4091ef560afac
qgcloud <vpnxyf@outlook.com>
2024-12-17 00:39:28 +0800

    Update setup_chinese.sh

--- setup_chinese.sh
+++ setup_chinese.sh
@@ -8,24 +8,6 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 
-# 检查脚本是否已经存在
-if [ -f ~/setup_chinese.sh ]; then
-    echo "检测到旧脚本，正在删除..."
-    rm ~/setup_chinese.sh
-fi
-
-# 下载新的脚本
-echo "下载新的设置中文显示脚本..."
-wget -qO ~/setup_chinese.sh https://raw.githubusercontent.com/qgcloud/base/main/setup_chinese.sh
-
-# 给予执行权限
-chmod +x ~/setup_chinese.sh
-
-# 执行脚本
-echo "执行设置中文显示脚本..."
-~/setup_chinese.sh
-
-
 # 更新系统包列表
 echo "更新系统包列表..."
 apt update
