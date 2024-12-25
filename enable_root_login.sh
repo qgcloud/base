2a55cc65797280453e2cbe635582503de208b008
qgcloud <vpnxyf@outlook.com>
2024-12-25 22:22:18 +0800

    Update enable_root_login.sh

--- enable_root_login.sh
+++ enable_root_login.sh
@@ -14,31 +14,41 @@ cp /root/.ssh/authorized_keys /root/.ssh/authorized_keys.bak
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 cp /etc/ssh/sshd_config.d/60-cloudimg-settings.conf /etc/ssh/sshd_config.d/60-cloudimg-settings.conf.bak
 
-# 编辑authorized_keys文件，将ssh-rsa之前的字符全部删掉
-if ! sed -i '/^ssh-rsa/i \\\n' /root/.ssh/authorized_keys && sed -i '/^ssh-rsa/q;s/^/#/' /root/.ssh/authorized_keys; then
+# 编辑authorized_keys文件，在第一个ssh-rsa之前插入换行符，并注释掉之前的所有行
+if ! sed -i '/^ssh-rsa/i \\\n' /root/.ssh/authorized_keys; then
     echo "编辑/root/.ssh/authorized_keys文件失败" 1>&2
     restore_backup
     exit 1
 fi
-
-# 编辑sshd_config文件
-if ! sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config; then
-    echo "编辑/etc/ssh/sshd_config文件失败，替换PermitRootLogin配置" 1>&2
-    restore_backup
-    exit 1
-fi
-if ! sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config; then
-    echo "编辑/etc/ssh/sshd_config文件失败，替换PasswordAuthentication配置" 1>&2
+if ! sed -i '/^ssh-rsa/q;s/^/#/' /root/.ssh/authorized_keys; then
+    echo "注释/root/.ssh/authorized_keys文件中的行失败" 1>&2
     restore_backup
     exit 1
 fi
 
-# 编辑60-cloudimg-settings.conf文件
+# 编辑sshd_config文件，添加PermitRootLogin yes和PasswordAuthentication yes
+if ! grep -q 'PermitRootLogin yes' /etc/ssh/sshd_config; then
+    if ! echo "PermitRootLogin yes" >> /etc/ssh/sshd_config; then
+        echo "添加PermitRootLogin yes到/etc/ssh/sshd_config失败" 1>&2
+        restore_backup
+        exit 1
+    fi
+fi
+if ! grep -q 'PasswordAuthentication yes' /etc/ssh/sshd_config; then
+    if ! echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config; then
+        echo "添加PasswordAuthentication yes到/etc/ssh/sshd_config失败" 1>&2
+        restore_backup
+        exit 1
+    fi
+fi
+
+# 编辑60-cloudimg-settings.conf文件，将PasswordAuthentication no替换为PasswordAuthentication yes
 if ! sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf; then
     echo "编辑/etc/ssh/sshd_config.d/60-cloudimg-settings.conf文件失败" 1>&2
     restore_backup
     exit 1
 fi
+
 
 # 重启SSH服务
 if systemctl restart sshd.service &>/dev/null; then
