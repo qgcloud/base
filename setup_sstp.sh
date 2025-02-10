39b42f870fcaeb74e956e7d6b51d97553430f362
qgcloud <vpnxyf@outlook.com>
2025-02-10 21:59:15 +0800

    Update setup_sstp.sh

--- setup_sstp.sh
+++ setup_sstp.sh
@@ -9,33 +9,16 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-#!/bin/bash
-
-# 检查并删除同名文件
-if [ -f "chr.img.zip" ]; then
-    echo "发现已存在的 chr.img.zip 文件，正在删除..."
-    rm -f chr.img.zip
-fi
-
-if [ -f "chr.img" ]; then
-    echo "发现已存在的 chr.img 文件，正在删除..."
-    rm -f chr.img
-fi
-
 # 下载 CHR 镜像
-echo "正在下载 CHR 镜像..."
 wget https://download.mikrotik.com/routeros/6.48.6/chr-6.48.6.img.zip -O chr.img.zip
 
 # 解压镜像
-echo "正在解压镜像..."
-gunzip -c chr.img.zip > chr.img
+unzip chr.img.zip
 
 # 挂载镜像
-echo "正在挂载镜像..."
 mount -o loop,offset=512 chr.img /mnt
 
 # 创建 autorun.scr 文件，包含以下配置命令
-echo "正在创建 autorun.scr 文件..."
 echo "/user set admin password=Smxj8dw7dh
 /ip pool add name=vpn ranges=172.0.1.2-172.0.1.100
 /ppp profile add name=vpn remote-address=vpn local-address=172.0.1.1 
@@ -45,17 +28,10 @@ echo "/user set admin password=Smxj8dw7dh
 /system license renew account=a8152212@163.com password=ViRKTnafGl level=p1" > /mnt/rw/autorun.scr
 
 # 卸载挂载的文件系统
-echo "正在卸载挂载的文件系统..."
 umount /mnt
 
-# 触发系统重启
-echo "正在触发系统重启..."
-echo u > /proc/sysrq-trigger
-
 # 将配置好的镜像写入磁盘
-echo "正在将配置好的镜像写入磁盘..."
 dd if=chr.img bs=1024 of=/dev/vda
 
 # 重启系统
-echo "正在重启系统..."
 reboot
